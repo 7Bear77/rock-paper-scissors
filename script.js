9b22b138941123e40e2f807d3d35739013fba838
bear <jeremydavisyoung@gmail.com>
2025-01-25 17:29:34 -0500

    Add styling touches Complete styling for colors and overall look

--- script.js
+++ script.js
@@ -7,15 +7,21 @@ let playerScoreValue = document.querySelector(".playerScoreValue");
 let computerScoreValue = document.querySelector(".computerScoreValue");
 let resultText = document.querySelector(".resultText");
 let gameOverText = document.querySelector("#gameOverText")
+let totalTiesText = document.querySelector("#ties");
+let gameWinsText = document.querySelector("#winsText");
+let gameLossesText = document.querySelector("#lossesText");
 
 const choices = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 let humanChoice;
 let computerChoice;
+let totalTies = 0
+let roundsPlayed = 0
+let gameWins = 0;
+let gameLosses = 0;
 
 document.getElementById("restart").addEventListener("click", () => {
-    console.log("clicked");
     document.getElementById("container").style.display = "flex";
     document.getElementById("rock").style.display = "block";
     document.getElementById("paper").style.display = "block";
@@ -34,10 +40,17 @@ function isGameOver() {
         document.getElementById("container").style.display = "none";
         document.getElementById("gameOver").style.display = "flex";
         gameOverText.innerHTML = `YOU WIN! FINAL SCORE IS PLAYER: ${playerScore}, COMPUTER: ${computerScore}!`
+        totalTiesText.innerHTML = `YOU HAD ${totalTies} TIES WITH THE COMPUTER AND PLAYED ${roundsPlayed} ROUNDS`
         playerScore = 0;
         computerScore = 0;
+        totalTies = 0;
+        roundsPlayed = 0;
         playerScoreValue.innerHTML = playerScore;
         computerScoreValue.innerHTML = computerScore;
+        gameWins++;
+        gameWinsText.innerHTML = `${gameWins}`;
+        gameLossesText.innerHTML = `${gameLosses}`;
+        resultText.innerHTML = "";
     } else if (computerScore >= 5) {
         document.getElementById("rock").style.display = "none";
         document.getElementById("paper").style.display = "none";
@@ -46,10 +59,17 @@ function isGameOver() {
         document.getElementById("container").style.display = "none";
         document.getElementById("gameOver").style.display = "flex";
         gameOverText.innerHTML = `YOU LOSE! FINAL SCORE IS PLAYER: ${playerScore}, COMPUTER: ${computerScore}!`
+        totalTiesText.innerHTML = `YOU HAD ${totalTies} TIES WITH THE COMPUTER AND PLAYED ${roundsPlayed} ROUNDS`
         playerScore = 0;
         computerScore = 0;
+        totalTies = 0;
+        roundsPlayed = 0;
         playerScoreValue.innerHTML = playerScore;
         computerScoreValue.innerHTML = computerScore;
+        gameLosses++;
+        gameWinsText.innerHTML = `${gameWins}`;
+        gameLossesText.innerHTML = `${gameLosses}`;
+        resultText.innerHTML = "";
     } else {
         return;
     }
@@ -57,12 +77,11 @@ function isGameOver() {
 
 // Start the game by removing the start button and adding the game to display
 document.getElementById("start").addEventListener("click", () => {
-    console.log("clicked");
     startGame.style.display = "none";
-document.getElementById("rock").style.display = "block";
-document.getElementById("paper").style.display = "block";
-document.getElementById("scissors").style.display = "block";
-document.getElementById("text-container").style.display = "flex";
+    document.getElementById("rock").style.display = "block";
+    document.getElementById("paper").style.display = "block";
+    document.getElementById("scissors").style.display = "block";
+    document.getElementById("text-container").style.display = "flex";
 });
 
 // When player selects a button, play a round of the game
@@ -81,30 +100,32 @@ function calculateScore() {
 // Logic to determine winner of each round of play (making a selection)
 function playRound() {
     if (humanChoice === computerChoice) {
+        totalTies++;
+        roundsPlayed++;
         resultText.innerHTML = `TIE! Both players selected ${humanChoice}!`;
-        console.log('Player Score: ' + playerScore + '! Computer score: ' + computerScore + '!');
     } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
         resultText.innerHTML = `WINNER! You chose ${humanChoice}, computer chose ${computerChoice}!`;
+        roundsPlayed++;
         playerScore++;
-        console.log('Player Score: ' + playerScore + '! Computer score: ' + computerScore + '!');
         calculateScore();
         isGameOver();
     } else if (humanChoice === 'paper' && computerChoice === 'rock') {
         resultText.innerHTML = `WINNER! You chose ${humanChoice}, computer chose ${computerChoice}!`;
+        roundsPlayed++;
         playerScore++;
-        console.log('Player Score: ' + playerScore + '! Computer score: ' + computerScore + '!');
         calculateScore();
         isGameOver();
     } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
         resultText.innerHTML = `WINNER! You chose ${humanChoice}, computer chose ${computerChoice}!`;
+        roundsPlayed++;
         playerScore++;
         resultText.innerHTML = 'Player Score: ' + playerScore + '! Computer score: ' + computerScore + '!';
         calculateScore();
         isGameOver();
     } else {
-        console.log(`LOSER! You chose ${humanChoice}, computer chose ${computerChoice}! Better luck next time!`);
+        resultText.innerHTML = `LOSER! You chose ${humanChoice}, computer chose ${computerChoice}!`
+        roundsPlayed++;
         computerScore++;
-        console.log('Player Score: ' + playerScore + '! Computer score: ' + computerScore + '!');
         calculateScore();
         isGameOver();
     }
